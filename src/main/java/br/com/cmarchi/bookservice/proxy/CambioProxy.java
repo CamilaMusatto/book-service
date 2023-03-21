a005ca2cd1e796b9323641ffb705d14b6d28e973
Camila Musatto Messias Marchi <camila.marchi@sysmap.com.br>
2023-03-21 18:12:33 -0300

    Distributed Tracing with Docker, Zipkin, Eureka and Sleuth

--- src/main/java/br/com/cmarchi/bookservice/proxy/CambioProxy.java
+++ src/main/java/br/com/cmarchi/bookservice/proxy/CambioProxy.java
@@ -2,11 +2,12 @@ package br.com.cmarchi.bookservice.proxy;
 
 import br.com.cmarchi.bookservice.response.Cambio;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
-
+@Component
 @FeignClient(name = "cambio-service")
 public interface CambioProxy {
     @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
